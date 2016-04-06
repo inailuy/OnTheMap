c64cf25b123564bdb69ba06742e2acb7987baee4
iNailuY <inailuy@me.com>
2016-04-06 12:42:45 -0500

    beginning progress - added view controllers, started with LoginVC

---
+++ On the Map/LoginVC.swift
@@ -0,0 +1,14 @@
+//
+//  LoginVC.swift
+//  On the Map
+//
+//  Created by inailuy on 4/5/16.
+//  Copyright © 2016 inailuy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginVC: UIViewController {
+
+}
---
+++ On the Map/MapVC.swift
@@ -0,0 +1,14 @@
+//
+//  MapVC.swift
+//  On the Map
+//
+//  Created by inailuy on 4/5/16.
+//  Copyright © 2016 inailuy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapVC: UIViewController {
+    
+}
